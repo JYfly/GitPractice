8ccdeed68028a6ce8689de3003afac9efd17142c
chen_sy <chen_sy@Ctrip.com>
2017-09-19 15:01:18 +0800

    Initial Commit

---
+++ GitPractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitPractice
+//
+//  Created by chen_sy on 2017/9/19.
+//  Copyright © 2017年 chen_sy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
